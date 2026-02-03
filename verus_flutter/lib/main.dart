f1256dcdce2e14033097b87d10320df543ad3b0f
TOMOKI977 <juliocesarsevericheorellana@gmail.com>
2026-02-03 14:51:45 -0400

    refactor(app): integrate AppTheme, AppLocale, and AppConfig into main entrypoint

--- verus_flutter/lib/main.dart
+++ verus_flutter/lib/main.dart
@@ -1,79 +1,43 @@
-import 'package:verus_client/verus_client.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'routing/app_router.dart';
+import 'package:verus_client/verus_client.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
-import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
 
-import 'screens/greetings_screen.dart';
+// Importa tus core utils
+import 'package:verus_flutter/core/config.dart';
+import 'package:verus_flutter/core/theme.dart';
+import 'package:verus_flutter/core/locale.dart';
 
-/// Sets up a global client object that can be used to talk to the server from
-/// anywhere in our app. The client is generated from your server code
-/// and is set up to connect to a Serverpod running on a local server on
-/// the default port. You will need to modify this to connect to staging or
-/// production servers.
-/// In a larger app, you may want to use the dependency injection of your choice
-/// instead of using a global client object. This is just a simple example.
 late final Client client;
 
-late String serverUrl;
-
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // When you are running the app on a physical device, you need to set the
-  // server URL to the IP address of your computer. You can find the IP
-  // address by running `ipconfig` on Windows or `ifconfig` on Mac/Linux.
-  //
-  // You can set the variable when running or building your app like this:
-  // E.g. `flutter run --dart-define=SERVER_URL=https://api.example.com/`.
-  //
-  // Otherwise, the server URL is fetched from the assets/config.json file or
-  // defaults to http://$localhost:8080/ if not found.
-  final serverUrl = await getServerUrl();
+  // Usa AppConfig para obtener la URL del servidor
+  final serverUrl = AppConfig.serverUrl;
 
-  // client = Client(serverUrl)
-  //   ..connectivityMonitor = FlutterConnectivityMonitor()
-  //   ..authSessionManager = FlutterAuthSessionManager();
+  client = Client(serverUrl)
+    ..connectivityMonitor = FlutterConnectivityMonitor();
 
-  // client.auth.initialize();
-
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(child: VerusApp()),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class VerusApp extends StatelessWidget {
+  const VerusApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Serverpod Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(title: 'Serverpod Example'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: const GreetingsScreen(),
-      // To test authentication in this example app, uncomment the line below
-      // and comment out the line above. This wraps the GreetingsScreen with a
-      // SignInScreen, which automatically shows a sign-in UI when the user is
-      // not authenticated and displays the GreetingsScreen once they sign in.
-      //
-      // body: SignInScreen(
-      //   child: GreetingsScreen(
-      //     onSignOut: () async {
-      //       await client.auth.signOutDevice();
-      //     },
-      //   ),
-      // ),
+    return MaterialApp.router(
+      title: AppConfig.appName,
+      routerConfig: appRouter,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      supportedLocales: AppLocale.supportedLocales,
+      localizationsDelegates: AppLocale.localizationsDelegates,
     );
   }
 }
